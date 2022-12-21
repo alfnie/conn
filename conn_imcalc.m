8251e0fec06c1ca9d91fe421cd59d3df0a009d3c
Alfonso Nieto-Castanon <alfnie@gmail.com>
2022-12-22 00:11:47 +0100

    misc

--- conn_imcalc.m
+++ conn_imcalc.m
@@ -81,7 +81,7 @@ if strcmp(fext,'.mat') % ROI-to-ROI files
                 case 'std', t=std(y,0,1);
                 case 'min', t=min(y,[],1);
                 case 'max', t=max(y,[],1);
-                case 'lin', t=pinv(op.DesignMatrix'*op.DesignMatrix)*op.DesignMatrix'*y; t=t(:,1).';
+                case 'lin', t=pinv(op.DesignMatrix'*op.DesignMatrix)*op.DesignMatrix'*y; t=t(1,:);
                 otherwise, error('unrecognized function keyword %s (only ''avg'' or ''std'' keywords allowed; use explicit function handle instead)',op.EvaluateFunction);
             end
         elseif nargin(op.EvaluateFunction)>1
