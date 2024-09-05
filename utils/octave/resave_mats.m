8b1543c4e88e98569f19b9b617bb1b118a168f41
tiborauer <tibor.auer@gmail.com>
2024-09-05 10:03:14 +0100

    ENH - function to re-generate OCTAVE-compatible mat-files

--- utils/octave/resave_mats.m
+++ utils/octave/resave_mats.m
@@ -1,10 +1,19 @@
 CONNDIR = fileparts(which('conn'));
 
-for fn = cellstr(spm_select('FPListRec',CONNDIR,'.*\.mat$'))'
-    fprintf('Processing %s...',fn{1});
-    vars = load(fn{1});
+for fn = dir(fullfile(CONNDIR,'**/*.mat'))'
+    fnfull = fullfile(fn.folder, fn.name);
+    fprintf('Processing %s...',fnfull);
+    
+    % Load content
+    vars = load(fnfull);
     nameVars = fieldnames(vars);
     cellfun(@(v) assignin('base',v,vars.(v)), nameVars);
-    save(fn{1},nameVars{:},'-v7')
+    
+    % Re-save
+    save(fnfull,nameVars{:},'-v7')
+    
+    % Clear
+    cellfun(@clear, nameVars);
+
     fprintf('Done!\n')
 end
